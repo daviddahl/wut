745ec6659da1dceaf63d2fe2551d24b21d10f362
David Dahl <ddahl@grove.co>
2020-05-20 18:05:24 -0400

    add some command handlers

--- main.js
+++ main.js
@@ -10,11 +10,34 @@ const IPFS = require('ipfs');
 const all = require('it-all');
 const blessed = require('blessed');
 const Room = require('ipfs-pubsub-room');
+const { v4: uuidv4 } = require('uuid');
 
-let topic = '__wut__chat__';
+const { box, secretbox, randomBytes } = require('tweetnacl');
+const  {
+  decodeUTF8,
+  encodeUTF8,
+  encodeBase64,
+  decodeBase64
+} = require('tweetnacl-util');
 
+const topic = '__wut__chat__';
+
+const configuration = {
+  handle: null,
+  bio: 'Web 3.0 Enthusiast',
+  homepage: 'https://github.com/daviddahl/wut',
+  myTopic: uuidv4(),
+  sharedKey: null,
+  keyPair: null,
+  peerProfiles: {},
+};
+
+let output,
+    input;
 
 async function main () {
+
+  configuration.keyPair = box.keyPair();
 
   const node = await IPFS.create();
   const version = await node.version();
@@ -36,10 +59,10 @@ async function main () {
     return process.exit(0);
   });
 
-  const output = blessed.Log({
+  output = blessed.Log({
     parent: screen,
     scrollable: true,
-    label: 'WUT: IPFS + TweetNacl Chat',
+    label: ' WUT: IPFS + TweetNacl Chat ',
     top: 0,
     left: 0,
     width: '60%',
@@ -60,7 +83,7 @@ async function main () {
   const peersList = blessed.Log({
     parent: screen,
     scrollable: true,
-    label: 'Peers',
+    label: ' Peers ',
     top: 0,
     left: '60%',
     width: '40%',
@@ -78,8 +101,8 @@ async function main () {
     }
   });
 
-  const input = blessed.Textbox({
-    label: 'Enter a message:',
+  input = blessed.Textbox({
+    label: ' Enter a message: ',
     parent: screen,
     top: 20,
     left: 0,
@@ -114,6 +137,14 @@ async function main () {
     switch (key.name) {
     case RETURN:
       let msg = input.getValue();
+
+      let comm = whichCommand(msg);
+
+      if (comm) {
+        commands[comm](msg, output);
+        return;
+      }
+
       room.broadcast(msg);
       // output.log(msg);
       screen.render();
@@ -142,14 +173,15 @@ async function main () {
   screen.append(output);
   screen.render();
 
-  output.log('welcome');
-  output.log('to');
-  output.log('WUT');
+  output.log('................Welcome');
+  output.log('...................To');
+  output.log('..................WUT\n');
+  output.log(`Your NaCl public key is: ${configuration.keyPair.publicKey}\n`);
   input.focus();
 
   output.log('IPFS node is initialized!');
   output.log('IPFS Version:', version.version);
-  output.log('IPFS Node Id:', nodeId);
+  output.log('IPFS Node Id:', nodeId.id);
 
   room.on('subscribed', () => {
     output.log(`Now connected to room: ${topic}`);
@@ -164,16 +196,96 @@ async function main () {
   });
 
   room.on('message', (message) => {
-    output.log(`${message.from}: ${message.data}`);
+    try {
+      let msg = JSON.parse(message.data);
+      if (msg.messageType) {
+        if (msg.messageType == 'profile') {
+          return output.log(`*** Profile broadcast: ${message.from} is now ${msg.handle}`);
+        }
+      }
+    } catch (ex) {}
+    return output.log(`${message.from}: ${message.data}`);
   });
 
+  const broadcastProfile = () => {
+    let profile = JSON.stringify({
+      messageType: 'profile',
+      handle: configuration.handle,
+      publicKey: configuration.keyPair.publicKey,
+      bio: configuration.bio,
+      id: nodeId.id,
+    });
+    room.broadcast(profile);
+  };
+
+
+  let peers = room.getPeers();
+  configuration.peers = peers;
+  peersList.log(`Peers: ${peers}`);
 
   let interval = setInterval(() => {
     let peers = room.getPeers();
+    configuration.peers = peers;
+    peersList.setContent('');
     peersList.log(`Peers: ${peers}`);
   }, 5000);
 
 }
+
+function invitePeerToPrivateRoom(peerId) {
+  // Invite a peer to a private, encrypted chat
+
+}
+
+const whichCommand = (input) => {
+  // output.log(`**** INPUT: ${input}`);
+  if (!input.startsWith('/')) {
+    return null;
+  }
+
+  let firstSpace = input.indexOf(' ');
+  if (firstSpace == -1) {
+    return null;
+  }
+
+  let comm = input.substring(0, firstSpace);
+  // output.log(`comm: ${comm}`);
+  switch (comm) {
+  case '/handle':
+    return 'handle';
+    break;
+  case '/peer':
+    return 'peer';
+    break;
+  default:
+    return null;
+  }
+};
+
+const handle = (data, output) => {
+  data = data.split(" ");
+  if (data.length != 2) {
+    output.log(`*** ERR: invalid input for /handle: ${input}`);
+    input.clearValue();
+    return;
+  }
+
+  configuration.handle = data[1];
+  output.log(`*** your /handle is now ${data[1]}`);
+  input.clearValue();
+};
+
+const peer = (data, output) => {
+  // peer commands
+
+};
+
+const commands = {
+  handle: handle,
+  peer: peer,
+};
+
+
 
 process.on('uncaughtException', (error) => {
   console.log(error);
