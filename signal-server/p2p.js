b39b9e7c735b234ff4df04885e16e2e358515c20
David Dahl <ddahl@nulltxt.se>
2020-05-31 15:01:12 -0400

    adding server module

---
+++ signal-server/p2p.js
@@ -0,0 +1,150 @@
+const TCP = require('libp2p-tcp');
+const Websockets = require('libp2p-websockets');
+const PeerInfo = require('peer-info')
+const Libp2p = require('libp2p')
+const WebRTCStar = require('libp2p-webrtc-star')
+const MPLEX = require('libp2p-mplex')
+const SECIO = require('libp2p-secio')
+const wrtc = require('wrtc')
+const GossipSub = require('libp2p-gossipsub')
+const MulticastDNS = require('libp2p-mdns')
+const Bootstrap = require('libp2p-bootstrap')
+const DHT = require('libp2p-kad-dht')
+const SignalingServer = require('libp2p-webrtc-star/src/sig-server')
+const demoIdJson = require('./demoId.json')
+
+const transportKey = WebRTCStar.prototype[Symbol.toStringTag]
+
+let idJSON;
+
+try {
+  idJSON = {
+    id: process.env.WUT_SIGNAL_SERVER_CID,
+    privKey: process.env.WUT_SIGNAL_SERVER_PRIV_KEY,
+    pubKey: process.env.WUT_SIGNAL_SERVER_PUB_KEY
+  }
+} catch (ex) {
+  console.error(ex)
+  console.error(ex.stack)
+  idJSON = demoIdJson
+}
+
+debugger;
+
+let ssAddr;
+
+// FINE GRAIN CONFIG OPTIONS
+const MDNS_INTERVAL_MS = 5000 // TODO: make this configurable via env vars or db records
+const CONNECTION_MGR_POLL_MS = 5000
+const RELAY_ENABLED = true
+const HOP_ENABLED = true
+const DHT_ENABLED = true
+const RANDOM_WALK_ENABLED = true
+const PUBSUB_ENABLED = true
+const METRICS_ENABLED = true
+
+const signalServerIP = () => {
+  if (!process.env.SIGNAL_SERVER_IP) {
+    throw new Error('process.env.SIGNAL_SERVER_IP required: without a signaling server, p2p peer discovery will not work');
+  }
+  return process.env.SIGNAL_SERVER_IP
+};
+
+const signalServerCID = () => {
+  if (!process.env.SIGNAL_SERVER_CID) {
+    throw new Error('process.env.SIGNAL_SERVER_CID required: without the signaling server CID, peer discovery will not work');
+  }
+  return process.env.SIGNAL_SERVER_CID
+};
+
+const signalServerPort = '15555'
+
+const getPeerInfo = async () => {
+  return PeerInfo.create(idJSON)
+}
+
+const libp2pBundle = async (opts) => {
+  // TODO: use opts to make things more configurable
+  const peerInfo = await getPeerInfo()
+
+  // Wildcard listen on TCP and Websocket
+  peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/63785')
+  peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/63786/ws')
+
+  const signalingServer = await SignalingServer.start({
+    port: 15555
+  })
+  ssAddr = `/ip4/${signalingServer.info.host}/tcp/${signalingServer.info.port}/ws/p2p-webrtc-star`
+  console.info(`Signaling server running at ${ssAddr}`)
+  peerInfo.multiaddrs.add(`${ssAddr}/p2p/${peerInfo.id.toB58String()}`)
+
+  debugger;
+
+  return new Libp2p({
+    peerInfo,
+    modules: {
+      transport: [ WebRTCStar, TCP, Websockets ],
+      streamMuxer: [ MPLEX ],
+      connEncryption: [ SECIO ],
+      pubsub: GossipSub,
+      peerDiscovery: [
+        MulticastDNS,
+        Bootstrap,
+      ]
+    },
+    config: {
+      relay: {           // Circuit Relay options (this config is part of libp2p core configurations)
+        enabled: true,   // Allows you to dial and accept relayed connections. Does not make you a relay.
+        hop: {
+          enabled: true, // Allows you to be a relay for other peers
+          active: true   // You will attempt to dial destination peers if you are not connected to them
+        }
+      },
+      EXPERIMENTAL: {
+        pubsub: true
+      },
+      autoDial: true, // auto dial to peers we find when we have less peers than `connectionManager.minPeers`
+      mdns: {
+        interval: MDNS_INTERVAL_MS,
+        enabled: true
+      },
+      peerDiscovery: {
+        // webRTCStar: {
+        //   enabled: true
+        // },
+        bootstrap: {
+          interval: 60e3,
+          enabled: true,
+          list: [
+            `/ip4/127.0.0.1/tcp/63785/ipfs/${peerInfo.id.toB58String()}`,
+          ]
+        },
+      },
+      transport: {
+        [transportKey]: {
+          wrtc
+        },
+      },
+      pubsub: {
+        enabled: true,
+        emitSelf: true,
+        signMessages: true,
+        strictSigning: true,
+      },
+      dht: {
+        enabled: false,
+        randomWalk: {
+          enabled: false
+        }
+      },
+    }
+  })
+}
+
+module.exports = {
+  libp2pBundle: libp2pBundle,
+  signalServerCID: signalServerCID,
+  signalServerIP: signalServerIP,
+  signalServerPort: signalServerPort,
+  ssAddr: ssAddr,
+}
---
+++ signal-server/server.js
@@ -0,0 +1,198 @@
+/*
+  ¿wut? : a very simple terminal-based chat application runing on top of IPFS, tweetnacl-js & blessed
+
+  Author: David Dahl <ddahl@nulltxt.se>
+*/
+
+'use strict';
+const PeerInfo = require('peer-info')
+const IPFS = require('ipfs');
+const Room = require('ipfs-pubsub-room');
+const wrtc = require('wrtc');
+const WStar = require('libp2p-webrtc-star');
+const TCP = require('libp2p-tcp');
+
+const all = require('it-all');
+const { v4: uuidv4 } = require('uuid');
+const { box } = require('tweetnacl');
+
+const { convertObjectToUint8 } = require('../lib/keys');
+const { Network } = require('../lib/network');
+const { logger } = require('../lib/logger');
+const {
+  DEFAULT_TOPIC,
+  APP_TITLE,
+  PEER_REFRESH_MS,
+  HOME_DIR,
+} = require('../lib/config');
+
+const {
+  libp2pBundle,
+  signalServerIP,
+  signalServerPort,
+  signalServerCID,
+  ssAddr,
+} = require('./p2p');
+
+var configuration = {
+  handle: null,
+  bio: 'Web 3.0 Enthusiast',
+  homepage: 'https://github.com/daviddahl/wut',
+  myTopic: uuidv4(),
+  sharedKey: null,
+  keyPair: null,
+  peerProfiles: {},
+};
+
+const e2eMessages = {};
+
+const storage = {
+  e2eMessages: e2eMessages,
+  configuration: configuration,
+  topic: DEFAULT_TOPIC,
+};
+
+async function main () {
+
+  let ipfsRepoPath = `${HOME_DIR}/`;
+  // create and expose main UI
+  let _keyPair = box.keyPair();
+  let pk = convertObjectToUint8(_keyPair.publicKey);
+  let sk = convertObjectToUint8(_keyPair.secretKey);
+
+  configuration.keyPair = { publicKey: pk, secretKey: sk };
+  debugger;
+  const p2p = await libp2pBundle()
+
+  const nodeId = p2p.peerInfo.id._idB58String
+
+  debugger;
+  await p2p.start()
+
+  const addrs = [
+    '/ip4/0.0.0.0/tcp/0',
+    '/ip4/0.0.0.0/tcp/0/ws',
+    '/ip4/0.0.0.0/tcp/9090/ws',
+    '/ip4/127.0.0.1/tcp/0/ws',
+    '/ip4/127.0.0.1/tcp/0'
+  ]
+
+  addrs.forEach((addr) => {
+    p2p.peerInfo.multiaddrs.add(addr)
+  })
+
+  const room = new Room(p2p, DEFAULT_TOPIC);
+  const network = new Network(configuration, nodeId, room)
+
+  // TODO: Display public key as QR CODE
+  console.log(`Your NaCl public key is: \n    ${configuration.keyPair.publicKey}\n`);
+
+  console.log(p2p.peerInfo.multiaddrs)
+  p2p.peerInfo.multiaddrs.forEach(ma => console.log(ma.toString()))
+
+  console.log('P2P node is initialized!');
+  console.log('P2P Node Id:', nodeId);
+
+  configuration.handle = nodeId;
+
+  console.log('\n...........................................');
+  console.log('................... Welcome ...............');
+  console.log('................... To ....................');
+  console.log(`.................. ${APP_TITLE} ..................`);
+  console.log('...........................................\n');
+  console.log('\n\n*** This is the LOBBY. It is *plaintext* group chat ***');
+  console.log('\n*** Type "/help" for help ***\n');
+
+  console.log(`\nP2P signal server: ${signalServerIP()}`);
+
+  p2p.on('peer:connect', (peer) => {
+    console.log('Connection established to:', peer.id.toB58String())	// Emitted when a peer has been found
+  })
+
+  // Emitted when a peer has been found
+  p2p.on('peer:discovery', (peer) => {
+    console.log('Discovered:', peer.id.toB58String())
+  })
+
+  room.on('subscribed', () => {
+    console.log(`Now connected to room: ${DEFAULT_TOPIC}`);
+  });
+
+  room.on('peer joined', (peer) => {
+    console.log(`Peer joined the room: ${peer}`);
+    let peerId = peer.id.toB58String()
+    if (peerId == nodeId) {
+      if (!configuration.handle) {
+        // set default for now
+        configuration.handle = peerId;
+      }
+      // Its YOU!
+      room.broadcastProfile();
+    }
+  });
+
+  room.on('peer left', (peer) => {
+    console.log(`Peer left: ${peer}`);
+  });
+
+  const DIRECT_MSG = 'dm';
+  const PROFILE_MSG = 'profile';
+  const BROADCAST_MSG = 'brodcast';
+
+  room.on('message', (message) => {
+    let msg;
+
+    try {
+      msg = JSON.parse(message.data);
+    } catch (ex) {
+      return console.log(`Error: Cannot parse badly-formed command.`);
+    }
+
+    if (msg.messageType) {
+      if (msg.messageType == PROFILE_MSG) {
+        // update peerprofile:
+        configuration.peerProfiles[message.from] = {
+          id: message.from,
+          handle: msg.handle.trim(),
+          bio: msg.bio,
+          publicKey: convertObjectToUint8(msg.publicKey),
+        };
+        return console.log(`*** Profile broadcast: ${message.from} is now ${msg.handle}`);
+      } else if (msg.messageType == DIRECT_MSG) {
+        console.log('Direct Message? wut?');
+        console.log(message.from, msg);
+      } else if (msg.messageType == BROADCAST_MSG) {
+        return console.log(`*** Broadcast: ${message.from}: ${msg.content}`);
+      }
+    }
+
+    // Handle peer refresh request
+    if (message.data == 'peer-refresh') {
+      network.broadcastProfile(message.from);
+    }
+
+    return console.log(`${message.from}: ${message.data}`);
+  });
+
+  let peers = room.getPeers();
+  configuration.peers = [peers];
+  if (peers.length) {
+    console.log('Peers:', configuration.peers)
+  }
+
+  let interval = setInterval(() => {
+    let peers = room.getPeers();
+    configuration.peers = [peers];
+    if (peers.length) {
+      console.log('Peers: ', configuration.peers);
+      screen.render();
+    }
+  }, PEER_REFRESH_MS);
+
+}
+
+// process.on('uncaughtException', (error) => {
+//   logger.error(error);
+// });
+
+main();
